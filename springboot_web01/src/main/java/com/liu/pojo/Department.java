b48c9e1cc96124b083cf6b1b0d4fa6fd8654fc26
liu <573296511@qq.com>
2020-08-07 09:13:52 +0800

    springboot web01提交到本地仓库

---
+++ springboot_web01/src/main/java/com/liu/pojo/Department.java
@@ -0,0 +1,13 @@
+package com.liu.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//部门表
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    private Integer id;
+    private String departmentName;
+}
